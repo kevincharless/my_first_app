8abfe80eca9aa9501d357ff8f6ce9604d68252de
Kevin charles <kevincharlesid@gmail.com>
2021-11-05 18:02:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./cc-uib-08.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>How to Git ?</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://cc.uib.ac.id/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          http://cc.uib.ac.id/
         </a>
       </header>
     </div>
